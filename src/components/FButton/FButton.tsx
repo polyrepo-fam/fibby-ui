3ef0631e9ded8f99904b1147cfe2bd363f442041
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-03-03 16:33:19 -0500

    fix: test1

--- src/components/FButton/FButton.tsx
+++ src/components/FButton/FButton.tsx
@@ -3,6 +3,6 @@ import { Button, ButtonProps } from '@jonbri/libby-ui';
 
 export interface FButtonProps extends ButtonProps {}
 export const FButton = ({ children }: FButtonProps) => {
-  const s = `${children}test-0`;
+  const s = `${children}test1`;
   return <Button>{s}</Button>;
 };
