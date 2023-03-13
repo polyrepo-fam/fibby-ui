73948655da6b2db13c44839e7e72e3b99a91c2e0
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-03-13 16:03:04 -0400

    fix: two

--- src/components/FButton/FButton.tsx
+++ src/components/FButton/FButton.tsx
@@ -3,6 +3,6 @@ import { Button, ButtonProps } from '@polyrepo-fam/libby-ui';
 
 export interface FButtonProps extends ButtonProps {}
 export const FButton = ({ children }: FButtonProps) => {
-  const s = `${children}test2`;
+  const s = `${children}test3`;
   return <Button>{s}</Button>;
 };
